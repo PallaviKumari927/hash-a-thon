6d1297ba2cc8c020538a57ad0417163a8c624105
Kumari,Pallavi <pallakumari@deloitte.com>
2023-07-22 16:53:02 +0530

    created Middleware for Role and error

--- routes/hackathon.js
+++ routes/hackathon.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllHackathon,addHackathon,deleteHackathon,updateHackathon,addParticipate,searchHackathon,getParticipate,getAllParticipate} = require("../controller/hackathonController");
+
+const {
+    getAllHackathon,
+    addHackathon,
+    deleteHackathon,
+    updateHackathon,
+    addParticipate,
+    searchHackathon,
+    getParticipate,
+    getAllParticipate
+} = require("../controller/hackathonController");
+
+const {
+    checkOrganizerRole,
+    checkEmployeeRole
+} = require('../middleware/role');
 
 router.route("/").get(getAllHackathon);
-router.route("/").post(addHackathon);
-router.route("/:id").delete(deleteHackathon);
-router.route("/:id").patch(updateHackathon);
+router.route("/").post(checkOrganizerRole, addHackathon);
+router.route("/:id").delete(checkOrganizerRole, deleteHackathon);
+router.route("/:id").patch(checkOrganizerRole, updateHackathon);
 
 router.route("/:id/participate").post(addParticipate)
 router.route("/:id/participate").get(getParticipate)
